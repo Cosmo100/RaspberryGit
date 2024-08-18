5713defb5fdc23a637161d55919c667e39d5ac4f
Cosmo100 <cosmo@heisopo.de>
2024-08-18 12:17:48 +0200

    Beschreibung geändert

--- Anleitung Git.py
+++ Anleitung Git.py
@@ -1,6 +1,7 @@
 #Dies ist kein Phyton-Programm, sondern eine Anleitung, wie Git-hub benutzt wird
 
 
+
 Dokumentation unter https://docs.github.com/de
 
 Beispiel am Raspberry:
@@ -9,19 +10,27 @@ Beispiel am Raspberry:
     password: tester10
     
     wir starten im Pfad /root
-    wechsele in Pfad "Schreibtisch"
+    wechsele in Pfad "Schreibtisch":
     cd Schreibtisch
-   
+    
     git add *.py
     git push --set-upstream origin main
     Meldung: Enter passphrase for key '/root/.ssh/id_ed25519':
     Eingabe Passwort: tester10
     
-    oder:
+    oder nach einer Änderung:
+     git status
+     git add *.py
      git commit -m "Noch eine Änderung"
      git push
      Meldung: Enter passphrase for key '/root/.ssh/id_ed25519':
      Eingabe Passwort: tester10
+     
     
     
-    Pfad root/Schreibtisch komplett auf Git Hub:
+
+Die wichtigsten Befehle:
+
+https://www.devguide.at/git/wichtigsten-befehle-fuer-ein-lokales-repository/
+
+git Status: Zeigt die geänderten Dateien
